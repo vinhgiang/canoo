ca5c5b05d0a2dc075c3cab6505215dcb9e665932
Vinh Giang <giangcamvinh@gmail.com>
2024-07-06 23:03:34 -0600

    finish places filter

--- lib/views/explore/widgets/places_filter.dart
+++ lib/views/explore/widgets/places_filter.dart
@@ -1,47 +1,79 @@
 import 'package:flutter/material.dart';
 
-class PlacesFilter extends StatelessWidget {
+class PlacesFilter extends StatefulWidget {
   const PlacesFilter({super.key});
 
   @override
+  State<PlacesFilter> createState() => _PlacesFilterState();
+}
+
+class _PlacesFilterState extends State<PlacesFilter> {
+  bool _isFilterChanged = false;
+  bool _isOnlyAccessibleVenues = true;
+
+  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.8,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-            color: Theme.of(context).colorScheme.onBackground,
-            child: Text(
-              'Filter venues',
-              style: Theme.of(context).textTheme.titleLarge,
+    return SafeArea(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.75,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+              color: Theme.of(context).colorScheme.onBackground,
+              child: Text(
+                'Filter venues',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
             ),
-          ),
-          Container(
-              padding: const EdgeInsets.all(20),
-              color: Theme.of(context).colorScheme.background,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+            Expanded(
+              child: Container(
+                  padding: const EdgeInsets.all(20),
+                  color: Theme.of(context).colorScheme.background,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Switch(
-                        value: true,
-                        activeColor: Colors.red,
-                        onChanged: (bool value) {},
-                      ),
-                      const SizedBox(width: 20),
-                      Text(
-                        'Show only venues I can access',
-                        style: Theme.of(context).textTheme.bodyLarge,
+                      Row(
+                        children: [
+                          Switch(
+                            value: _isOnlyAccessibleVenues,
+                            activeColor: Colors.red,
+                            onChanged: (bool value) {
+                              setState(() {
+                                _isOnlyAccessibleVenues = value;
+                                _isFilterChanged = true;
+                              });
+                            },
+                          ),
+                          const SizedBox(width: 20),
+                          Text(
+                            'Show only venues I can access',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        ],
                       ),
                     ],
-                  ),
-                ],
-              ))
-        ],
+                  )),
+            ),
+            if (_isFilterChanged)
+              Container(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Apply'),
+                    ),
+                  ],
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
