ed91a9d07636d9a4c81c3437c1db4234b209c48f
Vinh Giang <giangcamvinh@gmail.com>
2024-07-13 22:52:09 -0600

    fix places filter

--- lib/views/explore/widgets/places_filter.dart
+++ lib/views/explore/widgets/places_filter.dart
@@ -13,66 +13,65 @@ class _PlacesFilterState extends State<PlacesFilter> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.75,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-              color: Theme.of(context).colorScheme.onBackground,
-              child: Text(
-                'Filter venues',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.80,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+            color: Theme.of(context).colorScheme.onBackground,
+            child: Text(
+              'Filter venues',
+              style: Theme.of(context).textTheme.titleLarge,
             ),
-            Expanded(
-              child: Container(
-                  padding: const EdgeInsets.all(20),
-                  color: Theme.of(context).colorScheme.background,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Switch(
-                            value: _isOnlyAccessibleVenues,
-                            onChanged: (bool value) {
-                              setState(() {
-                                _isOnlyAccessibleVenues = value;
-                                _isFilterChanged = true;
-                              });
-                            },
-                          ),
-                          const SizedBox(width: 20),
-                          Text(
-                            'Show only venues I can access',
-                            style: Theme.of(context).textTheme.bodyLarge,
-                          ),
-                        ],
-                      ),
-                    ],
-                  )),
-            ),
-            if (_isFilterChanged)
-              Container(
+          ),
+          Expanded(
+            child: Container(
                 padding: const EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                color: Theme.of(context).colorScheme.background,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text('Apply'),
+                    Row(
+                      children: [
+                        Switch(
+                          value: _isOnlyAccessibleVenues,
+                          onChanged: (bool value) {
+                            setState(() {
+                              _isOnlyAccessibleVenues = value;
+                              _isFilterChanged = true;
+                            });
+                          },
+                        ),
+                        const SizedBox(width: 20),
+                        Text(
+                          'Show only venues I can access',
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                      ],
                     ),
                   ],
-                ),
-              )
-          ],
-        ),
+                )),
+          ),
+          if (_isFilterChanged)
+            Container(
+              padding: const EdgeInsets.all(20),
+              color: Theme.of(context).colorScheme.background,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Apply'),
+                  ),
+                ],
+              ),
+            )
+        ],
       ),
     );
   }
